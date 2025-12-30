7578d8e9aa4c096164ba5a4eb378595cd9b34035
Mohamed El-Gedawy <moehabsas@gmail.com>
2025-12-30 10:45:50 +0200

    chore(data): add topics with flashcards

--- lib/data.tsx
+++ lib/data.tsx
@@ -1,3 +1,11 @@
+import {
+  Siren,
+  DoorOpen,
+  FlaskConical,
+  Flame,
+  ArrowUpDown,
+} from "lucide-react";
+
 export interface Flashcard {
   slug: string;
   image: string;
@@ -14,4 +22,92 @@ export interface Topic {
   cards: Flashcard[];
 }
 
-export const topics: Topic[] = [];
+export const topics: Topic[] = [
+  {
+    slug: "emergency_codes",
+    title: "أكواد الطوارئ",
+    description: "تعلم المزيد عن جميع أكواد الطوارئ الخاصة بالمستشفيات.",
+    lastModified: new Date("30-12-2025"),
+    icon: Siren,
+    cards: [
+      {
+        slug: "emergency_codes",
+        title: "أكواد الطوارئ",
+        description: "تعرف علي معنى ألوان أكواد الطوارئ.",
+        image: "/images/cards/emergency_codes.jpeg",
+      },
+    ],
+  },
+  {
+    slug: "evacuation",
+    title: "إخلاء",
+    description: "تعلم المزيد عن إخلاء المستشفيات في أوقات الطوارئ.",
+    lastModified: new Date("30-12-2025"),
+    icon: DoorOpen,
+    cards: [
+      {
+        slug: "race",
+        title: "RACE",
+        description: "تعرف علي كيفية إخلاء المستشفيات في أوقات الطوارئ.",
+        image: "/images/cards/race.jpeg",
+      },
+    ],
+  },
+  {
+    slug: "chemical_signals",
+    title: "الإشارات الكيميائية",
+    description: "تعلم المزيد عن إشارات الكيميائية الخاصة بالمستشفيات.",
+    lastModified: new Date("30-12-2025"),
+    icon: FlaskConical,
+    cards: [
+      {
+        slug: "warning_signs_in_laboratories",
+        title: "إشارات تحذيرية في المختبرات",
+        description: "تعرف علي الإشارات التحذيرية في مختبرات المستشفيات.",
+        image: "/images/cards/warning_signs_in_laboratories.jpeg",
+      },
+    ],
+  },
+  {
+    slug: "fire_extinguisher",
+    title: "الطفايات",
+    description: "تعلم المزيد عن كيفية استخدام الطفايات والتعامل مع الحرائق.",
+    lastModified: new Date("30-12-2025"),
+    icon: Flame,
+    cards: [
+      {
+        slug: "race_pass",
+        title: "RACE_PASS",
+        description: "تعلم كيفية التعامل مع الحرائق في المستشفيات.",
+        image: "/images/cards/race_pass.jpeg",
+      },
+      {
+        slug: "fire_extinguisher_types",
+        title: "أنواع الطفايات",
+        description: "تعرف علي جميع أنواع الطفايات واستخداماتها.",
+        image: "/images/cards/fire_extinguisher_types.jpeg",
+      },
+      {
+        slug: "pass",
+        title: "استخدام الطفايات (PASS)",
+        description: "تعلم كيفية استخدام الطفايات في حالة حرائق بأمان تام.",
+        image: "/images/cards/pass.jpeg",
+      },
+    ],
+  },
+  {
+    slug: "elevators",
+    title: "المصاعد",
+    description: "تعلم المزيد عن كيفية استخدام المصاعد في المستشفيات.",
+    lastModified: new Date("30-12-2025"),
+    icon: ArrowUpDown,
+    cards: [
+      {
+        slug: "safety_in_elevators",
+        title: "السلامة في المصاعد",
+        description: "تعلم كيفية استخدام المصاعد بأمان في المستشفيات.",
+        image: "/images/cards/safety_in_elevators.jpeg",
+      },
+    ],
+  },
+];
